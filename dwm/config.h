9ef45be889aabafc412eb8350f6eb41b4067ecdc
Joshua Lee <josh@10leej.com>
2021-01-04 08:05:31 -0500

    backup dwm config, comment all the keybinds

--- dwm/config.h
+++ dwm/config.h
@@ -31,11 +31,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
+	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -69,44 +69,44 @@ static const char *editor[]  = { "vim", NULL };
 /* test */
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ControlMask,           XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} }, /* toggle the top status bar */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } }, /* change focus to the left */
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } }, /* change focus to the right */
+	{ MODKEY,                       XK_s,      incnmaster,     {.i = +1 } }, /* single app focus? */
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } }, /* not sure yet */
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, /* not sure yet*/
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, /* not sure yet*/
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} }, /* doesn't seem to work */
+	{ MODKEY,                       XK_Tab,    view,           {0} }, /* doesn't seem to work */
+	{ MODKEY,                       XK_q,      killclient,     {0} }, /* kill application */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* set tile layout */
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} }, /* set floating layout */
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} }, /* set fibonacci layout */
+	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[3]} }, /* set monocle layout */
+	{ MODKEY,                       XK_space,  setlayout,      {0} }, /* set to default tile layout, this can't change */
+	{ MODKEY|ControlMask,           XK_space,  togglefloating, {0} }, /* toggle window floating */
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } }, /* not sure yet */
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, /* not sure yet */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, /* not sure yet */
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } }, /* not sure yet */
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, /* not sure yet */
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, /* not sure yet*/
+	TAGKEYS(                        XK_1,                      0) /* switch to tag 1 */
+	TAGKEYS(                        XK_2,                      1) /* switch to tag 2 */
+	TAGKEYS(                        XK_3,                      2) /* switch to tag 3 */
+	TAGKEYS(                        XK_4,                      3) /* switch to tag 4 */
+	TAGKEYS(                        XK_5,                      4) /* switch to tag 5 */
+	TAGKEYS(                        XK_6,                      5) /* switch to tag 6 */
+	TAGKEYS(                        XK_7,                      6) /* switch to tag 7 */
+	TAGKEYS(                        XK_8,                      7) /* switch to tag 8 */
+	TAGKEYS(                        XK_9,                      8) /* switch to tag 9 */
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }, /* quit dwm */
 	/* App Launchers */
-	{ MODKEY,                       XK_b,      spawn,          {.v = web } },
-	{ MODKEY,                       XK_f,      spawn,          {.v = files } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = editor } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } }, /* open dmenu */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } }, /* open terminal */
+	{ MODKEY,                       XK_b,      spawn,          {.v = web } }, /* open web browser 1 */
+	{ MODKEY,                       XK_f,      spawn,          {.v = files } }, /* open file manager */
+	{ MODKEY,                       XK_e,      spawn,          {.v = editor } }, /* open text editor */
 };
 
 /* button definitions */
