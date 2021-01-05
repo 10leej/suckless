318b9b4c8d3850c05cc679fa6628134421439939
Joshua Lee <josh@10leej.com>
2021-01-05 10:38:13 -0500

    theme work

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;   /* border pixel of windows */
-static const unsigned int gappx     = 10;  /* gap pixel between windows */
+static const unsigned int gappx     = 6;  /* gap pixel between windows */
 static const unsigned int snap      = 32;  /* snap pixel */
 static const int showbar            = 1;   /* 0 means no bar */
 static const int topbar             = 1;   /* 0 means bottom bar */
@@ -18,7 +18,7 @@ static const char col_gray3[]       = "#bbbbbb";
 //current tag and current window font color
 static const char col_gray4[]       = "#eeeeee";
 //top bar second (default blue) color and active window border color
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#ae3f3e";
 //assign colors
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
