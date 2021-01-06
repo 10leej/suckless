9c52030d7ae90ca683099534a30c83907e0ff2a5
Joshua Lee <josh@10leej.com>
2021-01-06 17:28:35 -0500

    set some test tag rules

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -36,9 +36,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Pcmanfm",     NULL,       NULL,       0,            1,           -1 },
 	{ "Virt-manager",     NULL,       NULL,       0,            1,           -1 },
+	{ "Dungeondraft",     NULL,       NULL,       2,            1,           -1 },
+
 };
 
 /* layout(s) */
