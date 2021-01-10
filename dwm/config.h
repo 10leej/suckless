34feb4ecc58bd20285d931fa96d15bf0baf6b06b
Joshua Lee <josh@10leej.com>
2021-01-10 10:43:16 -0500

    config error

--- dwm/config.h
+++ dwm/config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+//	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Pcmanfm",     NULL,       NULL,       0,            1,           -1 },
 	{ "Virt-manager",     NULL,       NULL,       0,            1,           -1 },
 	{ "discord",     NULL,       NULL,       0,            0,           -2 },
